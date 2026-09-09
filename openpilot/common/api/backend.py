pdscomp/openpilot

feat: add fail-closed Connect backend policy

openpilot/common/api/backend.py
@@ -1,0 +1,125 @@
+from dataclasses import dataclass
+import os
+from pathlib import Path
+
+from openpilot.common.api.base import BaseApi
+from openpilot.common.params import Params
+
+
+@dataclass(frozen=True)
+class BackendConfig:
+  name: str
+  api_host: str
+  athena_host: str
+  pairing_host: str
+  dongle_param: str
+
+
+COMMA_BACKEND = BackendConfig(
+  "comma",
+  "https://api.commadotai.com",
+  "wss://athena.comma.ai",
+  "https://connect.comma.ai",
+  "CommaDongleId",
+)
+KONIK_BACKEND = BackendConfig(
+  "konik",
+  "https://api.konik.ai",
+  "wss://athena.konik.ai",
+  "https://stable.konik.ai",
+  "KonikDongleId",
+)
+
+
+def lock_sentinel(params: Params) -> Path:
+  return Path(params.get_param_path()).parent / ".konik_lockout"
+
+
+def create_lock_sentinel(params: Params) -> None:
+  path = lock_sentinel(params)
+  try:
+    fd = os.open(path, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o600)
+  except FileExistsError:
+    fd = os.open(path, os.O_RDWR)
+
+  try:
+    if os.fstat(fd).st_size == 0:
+      os.write(fd, b"1")
+    os.fsync(fd)
+  finally:
+    os.close(fd)
+
+  dir_fd = os.open(path.parent, os.O_RDONLY | getattr(os, "O_DIRECTORY", 0))
+  try:
+    os.fsync(dir_fd)
+  finally:
+    os.close(dir_fd)
+
+
+def is_konik_locked(params: Params) -> bool:
+  return lock_sentinel(params).exists() or params.get_bool("KonikLockout") or params.get_bool("TorqueInterceptorEnabled")
+
+
+def use_konik(params: Params) -> bool:
+  return is_konik_locked(params) or params.get_bool("UseKonikServer")
+
+
+def backend_config(params: Params) -> BackendConfig:
+  return KONIK_BACKEND if use_konik(params) else COMMA_BACKEND
+
+
+def put_bool_checked(params: Params, key: str, value: bool) -> None:
+  params.put_bool(key, value, block=True)
+  if params.get_bool(key) != value:
+    raise OSError(f"failed to persist {key}")
+
+
+def enforce_backend_state(params: Params) -> None:
+  if is_konik_locked(params):
+    create_lock_sentinel(params)
+    put_bool_checked(params, "KonikLockout", True)
+    put_bool_checked(params, "UseKonikServer", True)
+
+
+def enable_ti(params: Params) -> None:
+  create_lock_sentinel(params)
+  put_bool_checked(params, "KonikLockout", True)
+  put_bool_checked(params, "UseKonikServer", True)
+  put_bool_checked(params, "TorqueInterceptorEnabled", True)
+
+
+def request_ti_enable(params: Params) -> None:
+  put_bool_checked(params, "TorqueInterceptorEnableRequest", True)
+
+
+def finalize_ti_enable(params: Params) -> None:
+  if params.get_bool("TorqueInterceptorEnableRequest"):
+    enable_ti(params)
+    put_bool_checked(params, "TorqueInterceptorEnableRequest", False)
+
+
+def set_konik_enabled(params: Params, enabled: bool) -> None:
+  if not enabled and is_konik_locked(params):
+    raise RuntimeError("Konik cannot be disabled while locked; factory reset is required")
+  put_bool_checked(params, "UseKonikServer", enabled)
+
+
+def _active_dongle_id(params: Params, config: BackendConfig, allow_unregistered: bool) -> str | None:
+  dongle_id = params.get(config.dongle_param)
+  if dongle_id is None and not allow_unregistered:
+    raise RuntimeError(f"{config.name} backend requires {config.dongle_param}")
+  return dongle_id
+
+
+def active_dongle_id(params: Params, allow_unregistered: bool = False) -> str | None:
+  return _active_dongle_id(params, backend_config(params), allow_unregistered)
+
+
+def connect_client(params: Params, allow_unregistered: bool = False) -> tuple[BackendConfig, BaseApi]:
+  config = backend_config(params)
+  dongle_id = _active_dongle_id(params, config, allow_unregistered)
+  return config, BaseApi(dongle_id, config.api_host)
+
+
+def pairing_url(params: Params, token: str) -> str:
+  return f"{backend_config(params).pairing_host}/?pair={token}"

openpilot/common/api/tests/test_backend.py
@@ -1,0 +1,229 @@
+import os
+from pathlib import Path
+
+import pytest
+
+import openpilot.common.api.backend as backend
+from openpilot.common.api.backend import (
+  COMMA_BACKEND,
+  KONIK_BACKEND,
+  active_dongle_id,
+  backend_config,
+  connect_client,
+  enforce_backend_state,
+  finalize_ti_enable,
+  is_konik_locked,
+  lock_sentinel,
+  pairing_url,
+  request_ti_enable,
+  set_konik_enabled,
+  use_konik,
+)
+
+
+class FakeParams:
+  def __init__(self, root: Path, values=None):
+    self.values = values or {}
+    self.param_path = root / "params" / "d"
+    self.param_path.mkdir(parents=True)
+    self.writes = []
+    self.fail_on = None
+    self.silent_fail_on = None
+
+  def get_param_path(self):
+    return str(self.param_path)
+
+  def get_bool(self, key):
+    return bool(self.values.get(key, False))
+
+  def get(self, key):
+    return self.values.get(key)
+
+  def put_bool(self, key, value, block=False):
+    assert block
+    self.writes.append((key, value))
+    if key == self.fail_on:
+      raise OSError("injected write failure")
+    if key == self.silent_fail_on:
+      return
+    self.values[key] = value
+
+
+@pytest.mark.parametrize(("values", "expected"), [
+  ({}, "comma"),
+  ({"UseKonikServer": True}, "konik"),
+  ({"KonikLockout": True}, "konik"),
+  ({"TorqueInterceptorEnabled": True}, "konik"),
+])
+def test_effective_backend_matrix(tmp_path, values, expected):
+  assert backend_config(FakeParams(tmp_path, values)).name == expected
+
+
+def test_standalone_konik_can_disable_only_before_lock(tmp_path):
+  params = FakeParams(tmp_path)
+  set_konik_enabled(params, True)
+  set_konik_enabled(params, False)
+  assert not use_konik(params)
+
+  params.values["KonikLockout"] = True
+  with pytest.raises(RuntimeError, match="factory reset"):
+    set_konik_enabled(params, False)
+
+
+def test_ti_request_only_stages_activation(tmp_path):
+  params = FakeParams(tmp_path)
+  request_ti_enable(params)
+  assert params.values == {"TorqueInterceptorEnableRequest": True}
+  assert not is_konik_locked(params)
+
+
+def test_finalize_ti_enable_clears_request_last(tmp_path):
+  params = FakeParams(tmp_path, {"TorqueInterceptorEnableRequest": True})
+  finalize_ti_enable(params)
+  assert lock_sentinel(params).exists()
+  assert params.values == {
+    "TorqueInterceptorEnableRequest": False,
+    "KonikLockout": True,
+    "UseKonikServer": True,
+    "TorqueInterceptorEnabled": True,
+  }
+  assert params.writes[-1] == ("TorqueInterceptorEnableRequest", False)
+
+
+def test_partial_ti_enable_fails_closed_and_retries(tmp_path):
+  params = FakeParams(tmp_path, {"TorqueInterceptorEnableRequest": True})
+  params.fail_on = "KonikLockout"
+  with pytest.raises(OSError, match="injected"):
+    finalize_ti_enable(params)
+  assert lock_sentinel(params).exists()
+  assert use_konik(params)
+  assert params.get_bool("TorqueInterceptorEnableRequest")
+
+  params.fail_on = None
+  finalize_ti_enable(params)
+  assert params.get_bool("KonikLockout")
+  assert params.get_bool("UseKonikServer")
+  assert params.get_bool("TorqueInterceptorEnabled")
+  assert not params.get_bool("TorqueInterceptorEnableRequest")
+
+
+@pytest.mark.parametrize("failed_key", [
+  "KonikLockout",
+  "UseKonikServer",
+  "TorqueInterceptorEnabled",
+  "TorqueInterceptorEnableRequest",
+])
+def test_partial_ti_enable_detects_silent_write_failure(tmp_path, failed_key):
+  params = FakeParams(tmp_path, {"TorqueInterceptorEnableRequest": True})
+  params.silent_fail_on = failed_key
+
+  with pytest.raises(OSError, match=failed_key):
+    finalize_ti_enable(params)
+
+  assert lock_sentinel(params).exists()
+  assert use_konik(params)
+  assert params.get_bool("TorqueInterceptorEnableRequest")
+
+
+def test_disabling_ti_preserves_locked_backend(tmp_path):
+  params = FakeParams(tmp_path, {
+    "KonikLockout": True,
+    "UseKonikServer": True,
+    "TorqueInterceptorEnabled": True,
+  })
+  params.put_bool("TorqueInterceptorEnabled", False, block=True)
+  enforce_backend_state(params)
+  assert params.get_bool("KonikLockout")
+  assert params.get_bool("UseKonikServer")
+  assert use_konik(params)
+
+
+def test_enforce_repairs_tampered_locked_state(tmp_path):
+  params = FakeParams(tmp_path, {"KonikLockout": True, "UseKonikServer": False})
+  enforce_backend_state(params)
+  assert lock_sentinel(params).exists()
+  assert params.get_bool("UseKonikServer")
+
+
+def test_sentinel_is_durable_lock_source(tmp_path):
+  params = FakeParams(tmp_path)
+  lock_sentinel(params).touch()
+  assert is_konik_locked(params)
+  enforce_backend_state(params)
+  assert params.get_bool("KonikLockout")
+  assert params.get_bool("UseKonikServer")
+
+
+def test_locked_backend_requires_konik_identity(tmp_path):
+  params = FakeParams(tmp_path, {"KonikLockout": True})
+  with pytest.raises(RuntimeError, match="KonikDongleId"):
+    active_dongle_id(params)
+  with pytest.raises(RuntimeError, match="KonikDongleId"):
+    connect_client(params)
+  config, client = connect_client(params, allow_unregistered=True)
+  assert config == KONIK_BACKEND
+  assert client.dongle_id is None
+
+
+def test_connect_client_returns_one_backend_snapshot(tmp_path, monkeypatch):
+  params = FakeParams(tmp_path, {"CommaDongleId": "comma-id", "KonikDongleId": "konik-id"})
+  selections = iter((COMMA_BACKEND, KONIK_BACKEND))
+  monkeypatch.setattr(backend, "backend_config", lambda _: next(selections))
+  config, client = connect_client(params)
+  assert config == COMMA_BACKEND
+  assert client.api_host == COMMA_BACKEND.api_host
+  assert client.dongle_id == "comma-id"
+
+
+def test_lock_sentinel_is_fsynced_with_parent(tmp_path, monkeypatch):
+  params = FakeParams(tmp_path)
+  sentinel = lock_sentinel(params)
+  events = []
+  fd_paths = {}
+  real_open = os.open
+  real_write = os.write
+  real_fsync = os.fsync
+
+  def tracked_open(path, flags, mode=0o777):
+    fd = real_open(path, flags, mode)
+    fd_paths[fd] = Path(path)
+    events.append(("open", Path(path)))
+    return fd
+
+  def tracked_write(fd, data):
+    events.append(("write", fd_paths[fd]))
+    return real_write(fd, data)
+
+  def tracked_fsync(fd):
+    events.append(("fsync", fd_paths[fd]))
+    return real_fsync(fd)
+
+  monkeypatch.setattr(backend.os, "open", tracked_open)
+  monkeypatch.setattr(backend.os, "write", tracked_write)
+  monkeypatch.setattr(backend.os, "fsync", tracked_fsync)
+
+  backend.create_lock_sentinel(params)
+  assert sentinel.read_bytes() == b"1"
+  assert [event for event in events if event[0] == "write"] == [("write", sentinel)]
+  assert [event for event in events if event[0] == "fsync"] == [("fsync", sentinel), ("fsync", sentinel.parent)]
+
+  events.clear()
+  backend.create_lock_sentinel(params)
+  assert not [event for event in events if event[0] == "write"]
+  assert [event for event in events if event[0] == "fsync"] == [("fsync", sentinel), ("fsync", sentinel.parent)]
+
+
+def test_backend_endpoints_and_pairing_urls(tmp_path):
+  assert COMMA_BACKEND.api_host == "https://api.commadotai.com"
+  assert COMMA_BACKEND.athena_host == "wss://athena.comma.ai"
+  assert COMMA_BACKEND.pairing_host == "https://connect.comma.ai"
+  assert COMMA_BACKEND.dongle_param == "CommaDongleId"
+  assert KONIK_BACKEND.api_host == "https://api.konik.ai"
+  assert KONIK_BACKEND.athena_host == "wss://athena.konik.ai"
+  assert KONIK_BACKEND.pairing_host == "https://stable.konik.ai"
+  assert KONIK_BACKEND.dongle_param == "KonikDongleId"
+
+  params = FakeParams(tmp_path)
+  assert pairing_url(params, "token") == "https://connect.comma.ai/?pair=token"
+  set_konik_enabled(params, True)
+  assert pairing_url(params, "token") == "https://stable.konik.ai/?pair=token"

pdscomp/zoompilot

openpilot/common/api/backend.py
@@ -1,0 +1,115 @@
+from dataclasses import dataclass
+import os
+from pathlib import Path
+
+from openpilot.common.api.base import BaseApi
+from openpilot.common.params import Params
+
+
+@dataclass(frozen=True)
+class BackendConfig:
+  name: str
+  api_host: str
+  athena_host: str
+  pairing_host: str
+  dongle_param: str
+
+
+COMMA_BACKEND = BackendConfig(
+  "comma",
+  "https://api.commadotai.com",
+  "wss://athena.comma.ai",
+  "https://connect.comma.ai",
+  "CommaDongleId",
+)
+KONIK_BACKEND = BackendConfig(
+  "konik",
+  "https://api.konik.ai",
+  "wss://athena.konik.ai",
+  "https://stable.konik.ai",
+  "KonikDongleId",
+)
+
+
+def lock_sentinel(params: Params) -> Path:
+  return Path(params.get_param_path()).parent / ".konik_lockout"
+
+
+def create_lock_sentinel(params: Params) -> None:
+  path = lock_sentinel(params)
+  try:
+    fd = os.open(path, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o600)
+  except FileExistsError:
+    fd = os.open(path, os.O_RDWR)
+
+  try:
+    if os.fstat(fd).st_size == 0:
+      os.write(fd, b"1")
+    os.fsync(fd)
+  finally:
+    os.close(fd)
+
+  dir_fd = os.open(path.parent, os.O_RDONLY | getattr(os, "O_DIRECTORY", 0))
+  try:
+    os.fsync(dir_fd)
+  finally:
+    os.close(dir_fd)
+
+
+def is_konik_locked(params: Params) -> bool:
+  return lock_sentinel(params).exists() or params.get_bool("KonikLockout") or params.get_bool("KonikInterlock")
+
+
+def use_konik(params: Params) -> bool:
+  return is_konik_locked(params) or params.get_bool("UseKonikServer")
+
+
+def backend_config(params: Params) -> BackendConfig:
+  return KONIK_BACKEND if use_konik(params) else COMMA_BACKEND
+
+
+def put_bool_checked(params: Params, key: str, value: bool) -> None:
+  params.put_bool(key, value, block=True)
+  if params.get_bool(key) != value:
+    raise OSError(f"failed to persist {key}")
+
+
+def enforce_backend_state(params: Params) -> None:
+  if is_konik_locked(params):
+    create_lock_sentinel(params)
+    put_bool_checked(params, "KonikLockout", True)
+    put_bool_checked(params, "UseKonikServer", True)
+
+
+def enable_interlock(params: Params) -> None:
+  create_lock_sentinel(params)
+  put_bool_checked(params, "KonikLockout", True)
+  put_bool_checked(params, "UseKonikServer", True)
+  put_bool_checked(params, "KonikInterlock", True)
+
+
+def set_konik_enabled(params: Params, enabled: bool) -> None:
+  if not enabled and is_konik_locked(params):
+    raise RuntimeError("Konik cannot be disabled while locked; factory reset is required")
+  put_bool_checked(params, "UseKonikServer", enabled)
+
+
+def _active_dongle_id(params: Params, config: BackendConfig, allow_unregistered: bool) -> str | None:
+  dongle_id = params.get(config.dongle_param)
+  if dongle_id is None and not allow_unregistered:
+    raise RuntimeError(f"{config.name} backend requires {config.dongle_param}")
+  return dongle_id
+
+
+def active_dongle_id(params: Params, allow_unregistered: bool = False) -> str | None:
+  return _active_dongle_id(params, backend_config(params), allow_unregistered)
+
+
+def connect_client(params: Params, allow_unregistered: bool = False) -> tuple[BackendConfig, BaseApi]:
+  config = backend_config(params)
+  dongle_id = _active_dongle_id(params, config, allow_unregistered)
+  return config, BaseApi(dongle_id, config.api_host)
+
+
+def pairing_url(params: Params, token: str) -> str:
+  return f"{backend_config(params).pairing_host}/?pair={token}"

openpilot/common/api/tests/test_backend.py
@@ -1,0 +1,215 @@
+import os
+from pathlib import Path
+
+import pytest
+
+import openpilot.common.api.backend as backend
+from openpilot.common.api.backend import (
+  COMMA_BACKEND,
+  KONIK_BACKEND,
+  active_dongle_id,
+  backend_config,
+  connect_client,
+  enable_interlock,
+  enforce_backend_state,
+  is_konik_locked,
+  lock_sentinel,
+  pairing_url,
+  set_konik_enabled,
+  use_konik,
+)
+
+
+class FakeParams:
+  def __init__(self, root: Path, values=None):
+    self.values = values or {}
+    self.param_path = root / "params" / "d"
+    self.param_path.mkdir(parents=True)
+    self.writes = []
+    self.fail_on = None
+    self.silent_fail_on = None
+
+  def get_param_path(self):
+    return str(self.param_path)
+
+  def get_bool(self, key):
+    return bool(self.values.get(key, False))
+
+  def get(self, key):
+    return self.values.get(key)
+
+  def put_bool(self, key, value, block=False):
+    assert block
+    self.writes.append((key, value))
+    if key == self.fail_on:
+      raise OSError("injected write failure")
+    if key == self.silent_fail_on:
+      return
+    self.values[key] = value
+
+
+@pytest.mark.parametrize(("values", "expected"), [
+  ({}, "comma"),
+  ({"UseKonikServer": True}, "konik"),
+  ({"KonikLockout": True}, "konik"),
+  ({"KonikInterlock": True}, "konik"),
+])
+def test_effective_backend_matrix(tmp_path, values, expected):
+  assert backend_config(FakeParams(tmp_path, values)).name == expected
+
+
+def test_standalone_konik_can_disable_only_before_lock(tmp_path):
+  params = FakeParams(tmp_path)
+  set_konik_enabled(params, True)
+  set_konik_enabled(params, False)
+  assert not use_konik(params)
+
+  params.values["KonikLockout"] = True
+  with pytest.raises(RuntimeError, match="factory reset"):
+    set_konik_enabled(params, False)
+
+
+def test_enable_interlock_locks_backend(tmp_path):
+  params = FakeParams(tmp_path)
+  enable_interlock(params)
+  assert lock_sentinel(params).exists()
+  assert params.values == {
+    "KonikLockout": True,
+    "UseKonikServer": True,
+    "KonikInterlock": True,
+  }
+
+
+def test_partial_interlock_enable_fails_closed_and_recovers(tmp_path):
+  params = FakeParams(tmp_path)
+  params.fail_on = "KonikLockout"
+  with pytest.raises(OSError, match="injected"):
+    enable_interlock(params)
+  assert lock_sentinel(params).exists()
+  assert use_konik(params)
+
+  params.fail_on = None
+  enable_interlock(params)
+  assert params.get_bool("KonikLockout")
+  assert params.get_bool("UseKonikServer")
+  assert params.get_bool("KonikInterlock")
+
+
+@pytest.mark.parametrize("failed_key", [
+  "KonikLockout",
+  "UseKonikServer",
+  "KonikInterlock",
+])
+def test_partial_interlock_enable_detects_silent_write_failure(tmp_path, failed_key):
+  params = FakeParams(tmp_path)
+  params.silent_fail_on = failed_key
+
+  with pytest.raises(OSError, match=failed_key):
+    enable_interlock(params)
+
+  assert lock_sentinel(params).exists()
+  assert use_konik(params)
+
+
+def test_clearing_interlock_preserves_locked_backend(tmp_path):
+  params = FakeParams(tmp_path, {
+    "KonikLockout": True,
+    "UseKonikServer": True,
+    "KonikInterlock": True,
+  })
+  params.put_bool("KonikInterlock", False, block=True)
+  enforce_backend_state(params)
+  assert params.get_bool("KonikLockout")
+  assert params.get_bool("UseKonikServer")
+  assert use_konik(params)
+
+
+def test_enforce_repairs_tampered_locked_state(tmp_path):
+  params = FakeParams(tmp_path, {"KonikLockout": True, "UseKonikServer": False})
+  enforce_backend_state(params)
+  assert lock_sentinel(params).exists()
+  assert params.get_bool("UseKonikServer")
+
+
+def test_sentinel_is_durable_lock_source(tmp_path):
+  params = FakeParams(tmp_path)
+  lock_sentinel(params).touch()
+  assert is_konik_locked(params)
+  enforce_backend_state(params)
+  assert params.get_bool("KonikLockout")
+  assert params.get_bool("UseKonikServer")
+
+
+def test_locked_backend_requires_konik_identity(tmp_path):
+  params = FakeParams(tmp_path, {"KonikLockout": True})
+  with pytest.raises(RuntimeError, match="KonikDongleId"):
+    active_dongle_id(params)
+  with pytest.raises(RuntimeError, match="KonikDongleId"):
+    connect_client(params)
+  config, client = connect_client(params, allow_unregistered=True)
+  assert config == KONIK_BACKEND
+  assert client.dongle_id is None
+
+
+def test_connect_client_returns_one_backend_snapshot(tmp_path, monkeypatch):
+  params = FakeParams(tmp_path, {"CommaDongleId": "comma-id", "KonikDongleId": "konik-id"})
+  selections = iter((COMMA_BACKEND, KONIK_BACKEND))
+  monkeypatch.setattr(backend, "backend_config", lambda _: next(selections))
+  config, client = connect_client(params)
+  assert config == COMMA_BACKEND
+  assert client.api_host == COMMA_BACKEND.api_host
+  assert client.dongle_id == "comma-id"
+
+
+def test_lock_sentinel_is_fsynced_with_parent(tmp_path, monkeypatch):
+  params = FakeParams(tmp_path)
+  sentinel = lock_sentinel(params)
+  events = []
+  fd_paths = {}
+  real_open = os.open
+  real_write = os.write
+  real_fsync = os.fsync
+
+  def tracked_open(path, flags, mode=0o777):
+    fd = real_open(path, flags, mode)
+    fd_paths[fd] = Path(path)
+    events.append(("open", Path(path)))
+    return fd
+
+  def tracked_write(fd, data):
+    events.append(("write", fd_paths[fd]))
+    return real_write(fd, data)
+
+  def tracked_fsync(fd):
+    events.append(("fsync", fd_paths[fd]))
+    return real_fsync(fd)
+
+  monkeypatch.setattr(backend.os, "open", tracked_open)
+  monkeypatch.setattr(backend.os, "write", tracked_write)
+  monkeypatch.setattr(backend.os, "fsync", tracked_fsync)
+
+  backend.create_lock_sentinel(params)
+  assert sentinel.read_bytes() == b"1"
+  assert [event for event in events if event[0] == "write"] == [("write", sentinel)]
+  assert [event for event in events if event[0] == "fsync"] == [("fsync", sentinel), ("fsync", sentinel.parent)]
+
+  events.clear()
+  backend.create_lock_sentinel(params)
+  assert not [event for event in events if event[0] == "write"]
+  assert [event for event in events if event[0] == "fsync"] == [("fsync", sentinel), ("fsync", sentinel.parent)]
+
+
+def test_backend_endpoints_and_pairing_urls(tmp_path):
+  assert COMMA_BACKEND.api_host == "https://api.commadotai.com"
+  assert COMMA_BACKEND.athena_host == "wss://athena.comma.ai"
+  assert COMMA_BACKEND.pairing_host == "https://connect.comma.ai"
+  assert COMMA_BACKEND.dongle_param == "CommaDongleId"
+  assert KONIK_BACKEND.api_host == "https://api.konik.ai"
+  assert KONIK_BACKEND.athena_host == "wss://athena.konik.ai"
+  assert KONIK_BACKEND.pairing_host == "https://stable.konik.ai"
+  assert KONIK_BACKEND.dongle_param == "KonikDongleId"
+
+  params = FakeParams(tmp_path)
+  assert pairing_url(params, "token") == "https://connect.comma.ai/?pair=token"
+  set_konik_enabled(params, True)
+  assert pairing_url(params, "token") == "https://stable.konik.ai/?pair=token"